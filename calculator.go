9047ca9b04cb215bf3dff89c620859692a5324dd
AriSu2904 <arisusanto290401@gmail.com>
2023-10-15 22:36:48 +0700

    Divided Function

--- calculator.go
+++ calculator.go
@@ -1,5 +1,14 @@
 package calculator_module
 
+import "errors"
+
 func Sum(a int, b int) int {
 	return a + b
+}
+
+func Divide(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("Tidak bisa dibagi dengan 0!")
+	}
+	return a / b, nil
 }
